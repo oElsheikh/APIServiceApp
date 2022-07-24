2b50ea737edea86346abf643fb65c9bd581faa3f
oElsheikh <o.esmailg@gmail.om>
2022-07-24 14:07:03 +0200

    set debug to false

--- interview_app.py
+++ interview_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def tdidt_classifier(tree, header, instance):
         return leaf_label
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    port = os.environ.get("PORT", 5000)
+    app.run(debug= False, host= "0.0.0.0", port= port)
